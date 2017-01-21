6f14d1a4f35c209d32b1ea2f06c4b010ad643d63
mattstg <mattstger@gmail.com>
2017-01-20 23:28:34 -0500

    world size

--- Assets/Scripts/GV.cs
+++ Assets/Scripts/GV.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 public class GV : MonoBehaviour {
 
     public enum PillarType { Ground, Water}
-    public static readonly Vector3 World_Size = new Vector3(50, 10, 50);
+    public static readonly Vector2 World_Size = new Vector2(50, 50);
     public static readonly float Water_Sections = 4;
 }
